5a49987db66a3fed0ad6cb1043621633d380c856
javiersaez2 <asaez22.az@gmail.com>
2023-11-03 12:39:27 +0100

    Cambios y correeciones con la cancion y el artista y su animacion, y cambios en el boton de la letra corregidos y funcionando

--- image.php
+++ image.php
@@ -1,8 +1,6 @@
 <?php
 //Manejo de cache
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Content-Type: application/xml; charset=utf-8");
+
  flush();
 require 'simple_html_dom.php';
 
@@ -11,8 +9,8 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 
 
-$html3 = file_get_html('https://onlineradiobox.com/es/?cs=es.los401023');
-$datocancion2 = $html3->find('li[radioid=36569]');
+$html3 = file_get_html('https://onlineradiobox.com/es/genre/rap/');
+$datocancion2 = $html3->find('li[radioid=103333]');
 foreach ($datocancion2 as $datacancion2) {
     //Hacemos un find para busacar el nombre de la cancon y un segundo para reducir los datos y buscar luego el href
     $nombrecancion = $datacancion2->find('div[class=stations__station__track]', 0);
@@ -22,42 +20,47 @@ foreach ($datocancion2 as $datacancion2) {
 //Hacemos un explode por guion para poder separar la cancion del artista
 $arraycancion = explode("-", $nombrecancion);
 $nombrecancionfinal = "";
-
+$artistafinal ="";
 //Si tiene guion significa que tenemos una cancion con su artista, si no tendremos un texto generico
-if (str_contains($nombrecancion, '-')) {
+
     //Buscamos el href para luego usarlo para buscar la imagen
     preg_match_all('/<a[^>]+href=([\'"])(?<href>.+?)\1[^>]*>/i', $nombrecancion, $result);
+    if (!empty($result["href"][0])){
     $nombrecancioninagen = $result["href"][0];
-
-    //For para separar la cancion  y el artista
-    foreach ($arraycancion as $arraycancion) {
-        $nombrecancionfinal .= strip_tags($arraycancion) . "<br>";
     }
+    else{
+        $nombrecancioninagen ="";
+    }
+    //For para separar la cancion  y el artista
+    // var_dump($arraycancion);
 
-
-
+    $artistafinal .= "$arraycancion[0]"; 
+    // echo count($arraycancion);
+        if (count($arraycancion)==2){
+        $nombrecancionfinal = "$arraycancion[1]" ;
+        }
+    // echo"<br>".$nombrecancionfinal . "||||" . $arraycancion;
 
         $name = $nombrecancioninagen;
         $html2 = file_get_html('https://onlineradiobox.com' . $name);
-        // $html2 = file_get_html('https://onlineradiobox.com/track/432528191046634672/');
+        // $html2 = file_get_html('https://onlineradiobox.com/track/302248/');
 
         $dato2 = $html2->find('dl[class=subject]');
         foreach ($dato2 as $data2) {
 
             $cancion3 = $data2->find('dd[class=subject__cover--album]', 0);
+            if (!empty($cancion3)){
             $cancion3 = $cancion3->find('img', 0);
+            }
         }
 
-} else {
-    $nombrecancionfinal = 'No hay cancion';
-    $cancion3 = "Imagen";
-};
 
 
 
 
 
-$response['cancion'] = $nombrecancionfinal;     // not filled user or password
+$response['ncancion'] = $nombrecancionfinal;
+$response['acancion'] = $artistafinal;     // not filled user or password
 $cambiocancion = $data['cambiocancion'];
 
 //Si la cacnion cambio enviamos la imagen
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 data = ""
 Dato = "2";
 datosseparados = "";
+nombrecancion = ""
 const url = "https://timezone.abstractapi.com/v1/current_time/?api_key=5bcebd1be7a34ff7b57c1d5f3831f5d7&location=Oxford, United Kingdom"
 
 //Funcion llamada de api hora y dia y envio de datos a funcion "tiempo"
@@ -109,69 +110,176 @@ function tiempo(datos2) {
 
 i = 0
 //Cancion reload
+
+function cancionRadio() {
+  // $("#cancion22").load(' #cancion22');//actualizas el div
+  console.log(i = i + 10);
+  // track=document.querySelectorAll('.stations__station__track a');
+  console.log(
+    document.querySelectorAll('.stations__station__track a')[0]
+  )
+  var url = "image.php";
+
+
+
+  // if (!document.querySelectorAll('.stations__station__track a')[0].getAttribute("href")) {
+
+  // }
+  // else {
+  var data = { 'cambiocancion': 'no' };
+  cancicamb = "si"
+  // console.log(data);
+
+  //////////////////////////// -/ FETCH \- ///////////////////////////////////////
+  //////////////////////////// -| FETCH |- ///////////////////////////////////////
+  //////////////////////////// -\ FETCH /- ///////////////////////////////////////
+
+  fetch(url, {
+    method: 'POST', // or 'POST'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: { 'Content-Type': 'application/json' }  // input data
+  })
+    .then(res => res.json()).then(result => {
+      nombrecancion = result.ncancion
+      artistacancion = result.acancion
+      ncanciontexto = document.getElementsByClassName("ncancion")
+      acanciontexto = document.getElementsByClassName("acancion")
+      console.log(ncanciontexto.length)
+      for (i = 0; i < ncanciontexto.length; i++) {
+        ncanciontexto[i].innerHTML = nombrecancion + "&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;";
+      }
+      for (i = 0; i < acanciontexto.length; i++) {
+        acanciontexto[i].innerHTML = artistacancion + "&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;";
+      }
+      movertextoradio()
+
+      if (cancicamb == "si") {
+        document.getElementById("fotocancion").innerHTML = result.imagen
+      }
+      else {
+
+      }
+
+
+      document.getElementById("letra").addEventListener("click", () => {
+        artistacancionletra=artistacancion.replace(/(<([^>]+)>)/gi, "");
+        nombrecancionletra=nombrecancion.replace(/(<([^>]+)>)/gi, "");
+        window.open('http://google.com/search?q=' + nombrecancionletra +" " +  artistacancionletra +" lyrics");
+        // window.open('http://google.com/search?q='+);
+      })
+
+
+      console.log((result))
+    })
+  // }
+}
+
+cancionRadio()
 $(document).ready(function () {
   var refreshId = setInterval(function () {
-    // $("#cancion22").load(' #cancion22');//actualizas el div
-    console.log(i = i + 10);
-    // track=document.querySelectorAll('.stations__station__track a');
-    console.log(
-      document.querySelectorAll('.stations__station__track a')[0]
-    )
-    var url = "image.php";
-
-
-
-    // if (!document.querySelectorAll('.stations__station__track a')[0].getAttribute("href")) {
-       
-    // }
-    // else {
-      var data = { 'cambiocancion': 'no' };
-      cancicamb="si"
-      // console.log(data);
-      fetch(url, {
-        method: 'POST', // or 'POST'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers: { 'Content-Type': 'application/json' }  // input data
-      })
-        .then(res => res.json()).then(result => {
-          document.getElementById("cancion22").innerHTML=result.cancion
-          if(cancicamb=="si"){
-              document.getElementById("fotocancion").innerHTML = result.imagen
-          }
-          else{
-         
-          }
-          console.log((result))
-        })
-    // }
+    cancionRadio()
   }, 2500);
 
+
 });
 
 
+function movertextoradio() {
+  console.log(nombrecancion)
+  artcananima = artistacancion.length * 0.1
+  nombcancanima= nombrecancion.length * 0.1
+  // console.log(nombrecancion.width());
+
+  container = document.getElementsByClassName("textoradio")
+  containerartista = document.getElementsByClassName("artistatextoradio")
+  console.log($("#pixelradio").width())
+  console.log(container)
+
+  //Animacion de nombre de la cancion
+  nombcancanima = nombcancanima + 2
+  container[0].style.animationDuration = nombcancanima + "s";
+  if ($("#pixelradio").width() <= 300) {
+    container[0].style.webkitAnimationPlayState = "paused";
+    container[0].style.webkitAnimationName = 'asomename'; 
+    container[0].style.flexDirection = "column";
+    // alert("Funciona") 
+    
+  }
+  else {
+    container[0].style.webkitAnimationName = 'somename'; 
+    container[0].style.webkitAnimationPlayState = "running";
+  }
+  
+  //Animacion del artista/s de la cancion  
+  console.log($("#pixelartistaradio").width())
+  artcananima = artcananima + 2
+  containerartista[0].style.animationDuration = artcananima + "s";
+  if ($("#pixelartistaradio").width() <= 300) {
+    containerartista[0].style.webkitAnimationPlayState = "paused";
+    containerartista[0].style.webkitAnimationName = 'asomename'; 
+    containerartista[0].style.flexDirection = "column";
+
+    // alert("Funciona") 
+
+  }
+  else {
+    containerartista[0].style.webkitAnimationName = 'somename'; 
+    containerartista[0].style.webkitAnimationPlayState = "running";
+  }
+  console.log("a")
+}
 
 
 
 
 
-const buttons = Array.from(document.querySelectorAll("button"));
+buttons = document.getElementById("playpause");
+console.log(buttons)
 sonando = true
-var music = new Audio('http://playerservices.streamtheworld.com/api/livestream-redirect/LOS40.mp3');
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    console.log(sonando)
-    if (sonando){
-    btn.classList.toggle("active");
+var music = new Audio('https://21223.live.streamtheworld.com/LOS40_URBAN.mp3');
+buttons.addEventListener("click", () => {
+  console.log(sonando)
+  if (sonando) {
+    buttons.classList.toggle("active");
     music.play();
-    sonando=false
-    }
-    else{
-      btn.classList.toggle("active");
-      music.pause();
-      sonando=true
-    }
-  });
+    sonando = false
+  }
+  else {
+    buttons.classList.toggle("active");
+    music.pause();
+    sonando = true
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+texto1 = "Cupido"
+texto2 = "Rema-Rema (Halligans Live Rehearsal Version) Rema-Rema (Halligans Live Rehearsal Version)"
+texto3 = "a b c d e f g h i j k l m n ñ o p q r s t u v w x y z  1 2 3 4 5 6 7 8 9 0 0 1 9 8 7 6 5 4 3 2 1 z y x w v u y s r q p o n ñ m l k j i h g f e d c b aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+
+
+
+
+
+
+
+
 
 
 // <dd class="subject__cover--album"> 			<a href="/album/1682573331-clavaito---single" aria-label="Clavaíto - Single" class="ajax"> 			 				<img src="https://is5-ssl.mzstatic.com/image/thumb/Music126/v4/cc/ea/be/cceabe20-3b18-1ed5-3f6b-3438e27460c2/196871054959.jpg/100x100bb.jpg" alt="Clavaíto - Single" itemprop="image"> 			 			</a> 		</dd>
